271bbe6c9766af9e17fb94fc99bb5c82a636ea32
Oleh Kir <oleggio.kir@gmail.com>
2018-12-12 19:10:54 +0200

    [feature/nlp] Added files

---
+++ NLP/DialogFlowService.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+
+namespace NLP
+{
+    public class DialogFlowService : NLPService
+    {
+        private const String CLIENT_TOKEN = "4adc566a8281443c8c852a9f45976107";
+        private const String API_BASE_URL = "https://api.dialogflow.com/v1/";
+
+        private const String DEFAULT_LANG = "en";
+        private const String SESSION_ID = "88881466-112a-4349-a2f1-419177f6400c";
+
+
+        public NLPResponse Ask(string question)
+        {
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["query"] = question;
+
+            queryString["sessionId"] = "SESSION_ID";
+            queryString["lang"] = "DEFAULT_LANG";
+
+            var endpointUri = API_BASE_URL + "query" + "?" + queryString;
+
+            string responseStr = null;
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {CLIENT_TOKEN}");
+                httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
+
+                var response = httpClient.GetAsync(endpointUri).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    responseStr = response.Content.ReadAsStringAsync().Result;
+                }
+            }
+
+            if (responseStr == null)
+            {
+                return new NLPResponse();
+            }
+
+            return ParseResponse(responseStr);            
+        }
+
+        private NLPResponse ParseResponse(String responseStr)
+        {
+            dynamic response = JsonConvert.DeserializeObject(responseStr);
+
+            //TODO implement
+            return new NLPResponse();
+        }
+    }
+}
